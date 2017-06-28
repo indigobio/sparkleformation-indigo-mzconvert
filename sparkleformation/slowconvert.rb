97d8e4a562724004387bd5e3dd89291b1b3a3aa7
Greg Swallow <gswallow@gmail.com>
2017-06-28 11:52:22 -0400

    Create a pool of slow converters.

--- sparkleformation/slowconvert.rb
+++ sparkleformation/slowconvert.rb
@@ -1,18 +1,18 @@
 ENV['sg']                 ||= 'private_sg'
 ENV['chef_run_list']      ||= 'role[mzconvert]'
-ENV['lb_name']            ||= "#{ENV['org']}-#{ENV['environment']}-mzconvert-elb"
+ENV['lb_name']            ||= "#{ENV['org']}-#{ENV['environment']}-slowconvert-elb"
 ENV['notification_topic'] ||= "#{ENV['org']}_#{ENV['environment']}_deregister_chef_node"
 
-SparkleFormation.new('mzconvert').load(:base, :chef_base, :win2k8_ami, :ssh_key_pair, :git_rev_outputs).overrides do
+SparkleFormation.new('slowconvert').load(:base, :chef_base, :win2k8_ami, :ssh_key_pair, :git_rev_outputs).overrides do
   description <<"EOF"
-MZConvert EC2 instances, configured by Chef. ELB. Route53 record: mzconvert.#{ENV['private_domain']}.
+MZConvert EC2 instances, configured by Chef. ELB. Route53 record: slowconvert.#{ENV['private_domain']}.
 EOF
 
-  dynamic!(:iam_instance_profile, 'mzconvert',
+  dynamic!(:iam_instance_profile, 'slowconvert',
            :chef_bucket => registry!(:my_s3_bucket, 'chef')
           )
 
-  dynamic!(:elb, 'mzconvert',
+  dynamic!(:elb, 'slowconvert',
            :listeners => [
              { :instance_port => '80', :instance_protocol => 'tcp', :load_balancer_port => '80', :protocol => 'tcp'}
            ],
@@ -24,7 +24,7 @@ EOF
            :lb_name => ENV['lb_name']
   )
 
-  dynamic!(:launch_config, 'mzconvert',
+  dynamic!(:launch_config, 'slowconvert',
            :iam_instance_profile => 'MzconvertIAMInstanceProfile',
            :iam_role => 'MzconvertIAMRole',
            :create_ebs_volumes => false,
@@ -34,16 +34,16 @@ EOF
            :chef_run_list => ENV['chef_run_list']
           )
 
-  dynamic!(:auto_scaling_group, 'mzconvert',
-           :launch_config => :mzconvert_auto_scaling_launch_configuration,
+  dynamic!(:auto_scaling_group, 'slowconvert',
+           :launch_config => :slowconvert_auto_scaling_launch_configuration,
            :subnet_ids => registry!(:my_private_subnet_ids),
-           :load_balancers => _array(ref!(:mzconvert_elastic_load_balancing_load_balancer)),
+           :load_balancers => _array(ref!(:slowconvert_elastic_load_balancing_load_balancer)),
            :notification_topic => registry!(:my_sns_topics, ENV['notification_topic'])
           )
 
-  dynamic!(:record_set, 'mzconvert',
-           :record => 'mzconvert',
-           :target => :mzconvert_elastic_load_balancing_load_balancer,
+  dynamic!(:record_set, 'slowconvert',
+           :record => 'slowconvert',
+           :target => :slowconvert_elastic_load_balancing_load_balancer,
            :domain_name => ENV['private_domain'],
            :attr => 'DNSName',
            :ttl => '60'

11cf13e4a14d596aee3bdace69ed1b211995a327
Greg Swallow <gswallow@gmail.com>
2017-09-11 17:35:15 -0400

    Restore missing AMI (?) in us-east-2.

--- sparkleformation/components/win2k8_ami.rb
+++ sparkleformation/components/win2k8_ami.rb
@@ -1,7 +1,7 @@
 SparkleFormation.component(:win2k8_ami) do
   mappings(:region_to_ami) do
     set!('us-east-1'.disable_camel!, :ami => 'ami-8c7b2a9b')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-40f7ad25')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-863f1de3')
     set!('us-west-1'.disable_camel!, :ami => 'ami-acd890cc')
     set!('us-west-2'.disable_camel!, :ami => 'ami-d49035b4')
   end

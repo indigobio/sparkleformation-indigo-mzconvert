c953de0b8f420de6804cfb079cc65617c683caaf
Greg Swallow <gswallow@gmail.com>
2017-09-18 11:52:19 -0400

    Update base AMI.

--- sparkleformation/components/win2016_ami.rb
+++ sparkleformation/components/win2016_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.component(:win2016_ami) do
   mappings(:region_to_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-9e84b6e5')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-b76d4ed2')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-4d163c2d')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-ed09e495')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-56a8452c')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-1dd6f478')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-6ec3f50e')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-30619348')
   end
 end

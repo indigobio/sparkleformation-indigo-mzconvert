40361c6d69276c951012381808dccdf36d74a854
Greg Swallow <gswallow@gmail.com>
2017-09-21 17:44:14 -0400

    Update ami to include mzconvert 1.15.0 and web service 1.0.7.

--- sparkleformation/components/win2k8_ami.rb
+++ sparkleformation/components/win2k8_ami.rb
@@ -1,8 +1,8 @@
 SparkleFormation.component(:win2k8_ami) do
   mappings(:region_to_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-5303ea29')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-7e82a01b')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-3c54655c')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-1de61a65')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-3150b94b')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-1981a37c')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-e65f6e86')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-e6c73b9e')
   end
 end
